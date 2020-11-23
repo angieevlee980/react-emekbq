98494403992b9595c6ee09b560ead32c8f1468c0
angieevlee980 <avannesarestrepo@gmail.com>
2020-11-23 15:33:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-emekbq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-emekbq)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import Clock from "./Clock";
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
